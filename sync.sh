8bceea62cc12813d18c296aa5f96d57436eb1ca9
Keegan Mullaney <keeganmullaney@gmail.com>
2016-02-22 03:34:03 +0800

    commit before starting change to Ubuntu
     Changes to be committed:
    	modified:   lib/git.lib
    	modified:   sync.sh

--- sync.sh
+++ sync.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "*********************************************"
-echo "* A script to sync library files included in "
-echo "* various scripting projects using rsync     "
-echo "*                                            "
-echo "* Author : Keegan Mullaney                   "
-echo "* Company: KM Authorized LLC                 "
-echo "* Website: http://kmauthorized.com           "
-echo "*                                            "
-echo "* MIT: http://kma.mit-license.org            "
-echo "*********************************************"
+echo "# --------------------------------------------"
+echo "# A script to sync library files included in  "
+echo "# various scripting projects using rsync      "
+echo "#                                             "
+echo "# Author : Keegan Mullaney                    "
+echo "# Company: KM Authorized LLC                  "
+echo "# Website: http://kmauthorized.com            "
+echo "#                                             "
+echo "# MIT: http://kma.mit-license.org             "
+echo "# --------------------------------------------"
 
 LIB_DIR='includes'
-PROJECTS='linux-deploy-scripts2'
+PROJECTS='linux-deploy-scripts'
 #PROJECTS='linux-deploy-scripts2 middleman-html5-foundation/setup'
 
 source lib/base.lib
